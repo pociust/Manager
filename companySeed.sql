2e9cf961e61ed825c2a6990ef6c2361e4bbb8409
Tom Pocius <poci14@gmail.com>
2019-12-08 16:13:54 -0600

    added employee data to mysql

--- companySeed.sql
+++ companySeed.sql
@@ -24,7 +24,48 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name  VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
-  role_id INT NULL
-  manager_id INT,
+  role_id INT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO employee (first_name, last_name, role_id) values ("John", "Smith", "1");
+INSERT INTO employee (first_name, last_name, role_id) values ("Steven", "Jones", "2");
+INSERT INTO employee (first_name, last_name, role_id) values ("Sarah", "Kyle", "3");
+INSERT INTO employee (first_name, last_name, role_id) values ("Json", "File", "4");
+INSERT INTO employee (first_name, last_name, role_id) values ("Tim", "Lee", "5");
+INSERT INTO employee (first_name, last_name, role_id) values ("Luke", "Potter", "6");
+INSERT INTO employee (first_name, last_name, role_id) values ("Lula", "Quta", "7");
+INSERT INTO employee (first_name, last_name, role_id) values ("Hanz", "Tee", "8");
+INSERT INTO employee (first_name, last_name, role_id) values ("Ben", "Wyyat", "9");
+INSERT INTO employee (first_name, last_name, role_id) values ("Hope", "Hurr", "10");
+INSERT INTO employee (first_name, last_name, role_id) values ("Mike", "Pocius", "11");
+INSERT INTO employee (first_name, last_name, role_id) values ("Cody", "Sheraman", "12");
+
+
+
+INSERT INTO department (department_name) values ("Therapy");
+INSERT INTO department (department_name) values ("Nursing");
+INSERT INTO department (department_name) values ("Doctors");
+
+
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('Physical Therapist', '80000', 1);
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('Occupational Therapist', '65000', 1);
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('Speech Therapist', '60000', 1);
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('manager', '100000', 1);
+
+
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('Tele Nurse', '55000', 2);
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('ICU Nurse', '75000', 2);
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('Surgery Nurse', '90000', 2);
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('manager', '110000', 2);
+
+
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('PCP', '100000', 3);
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('Surgon', '200000', 3);
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('Specialist', '120000', 3);
+INSERT INTO employee_role (role_title, role_salary, department_id) values ('manager', '3000000', 3);
+
+
+SELECT first_name, last_name, role_title, role_salary, department_name FROM employee
+INNER JOIN employee_role ON employee.role_id = employee_role.id
+INNER JOIN  department ON employee_role.department_id = department.id
